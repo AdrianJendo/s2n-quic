fbe431c188b58b13897aefe1b6f09b16754ffb75
Adrian Jendo <ajendo@uwaterloo.ca>
2024-03-19 23:06:30 -0400

    Fix code to compile

--- quic/mock/src/bin.rs
+++ quic/mock/src/bin.rs
@@ -4,12 +4,21 @@ use mock::seamock;
 
 #[seamock]
 pub trait Test {
-    fn a(&self, z: bool) -> bool;
+    fn a(&self, _z: bool) -> bool;
     fn b(&self) -> u8;
     fn c(&self) -> i32;
 }
 
 
 pub fn main() {
-    // test();
+    let x = MockTest::new();
+
+    x.a(false, false);
+    x.a(false, false);
+    x.b(1);
+    x.c(32);
+
+    assert!(x.expect_times_a(2));
+    assert!(x.expect_times_b(1));
+    assert!(x.expect_times_c(1));
 }
--- quic/mock/src/lib.rs
+++ quic/mock/src/lib.rs
@@ -35,7 +35,7 @@ pub fn seamock(_args: TokenStream, input: TokenStream) -> TokenStream {
 
     let impl_methods = input.items.iter().filter_map(|item| {
         if let TraitItem::Method(method) = item {
-            let method_name = Ident::new(&format!("{}_with", &method.sig.ident), method.sig.ident.span());
+            let method_name = &method.sig.ident;
             let method_output = &method.sig.output;
             let ret_type: syn::Type = match &method.sig.output {
                 syn::ReturnType::Type(_, x) => *x.to_owned(),
@@ -44,9 +44,9 @@ pub fn seamock(_args: TokenStream, input: TokenStream) -> TokenStream {
             let method_inputs = &method.sig.inputs;
             let times_attr = Ident::new(&format!("times_{}", &method.sig.ident), method.sig.ident.span());
             Some (quote! {
-                fn #method_name(#method_inputs, _r: #ret_type) #method_output {
+                fn #method_name(#method_inputs, r: #ret_type) #method_output {
                     self.#times_attr.replace_with(|&mut old| old + 1);
-                    _r
+                    r
                 }
             })
         } else {
@@ -60,7 +60,7 @@ pub fn seamock(_args: TokenStream, input: TokenStream) -> TokenStream {
     let mock_struct = quote! {
         struct #mock_struct_name {
             #(
-                #x: std::cell::RefCell<u8>,
+                #x: std::cell::RefCell<u64>,
             )*
         }
     };
@@ -78,18 +78,18 @@ pub fn seamock(_args: TokenStream, input: TokenStream) -> TokenStream {
                 }
             }
             #(
-                fn #expect_trait_methods(&self, times: u8) -> bool {
-                    self.#x.borrow().clone() == times
+                fn #expect_trait_methods(&self, times: u64) -> bool {
+                    *self.#x.borrow() == times
                 }
             )*
             #(#impl_methods)*
         }
     };
 
-    let original = &input.ident;
-    let trait_impl = quote! {
-        impl #original for #mock_struct_name { }
-    };
+    // let original = &input.ident;
+    // let trait_impl = quote! {
+    //     impl #original for #mock_struct_name { }
+    // };
 
     // Combine the generated tokens
     let expanded = quote! {
@@ -97,50 +97,10 @@ pub fn seamock(_args: TokenStream, input: TokenStream) -> TokenStream {
         #input
         #mock_struct
         #mock_impl
-        #trait_impl
+        // #trait_impl
     };
+
 
     TokenStream::from(expanded)
 }
 
-
-
-// #[proc_macro_attribute]
-// pub fn show_streams(attr: TokenStream, item: TokenStream) -> TokenStream {
-//     println!("attr: \"{}\"", attr.to_string());
-//     println!("item: \"{}\"", item.to_string());
-//     item
-// }
-// #[proc_macro_attribute]
-// pub fn add_field(_args: TokenStream, input: TokenStream) -> TokenStream  {
-//
-//     println!("attr: \"{}\"", _args.to_string());
-//     println!("item: \"{}\"", input.to_string());
-//
-//     let args_parsed: Vec<String> = _args.to_string().split(", ").map(str::to_string).collect();
-//
-//     let mut ast = parse_macro_input!(input as DeriveInput);
-//     match &mut ast.data {
-//         syn::Data::Struct(ref mut struct_data) => {
-//             match &mut struct_data.fields {
-//                 syn::Fields::Named(fields) => {
-//                     for field in args_parsed.iter() {
-//                         // println!("{}", field);
-//                         // quote!({ pub, field, : u8 });
-//                         fields
-//                             .named
-//                             .push(syn::Field::parse_named.parse2(quote! { pub format!("{}", field): u8 }).unwrap());
-//                     }
-//                 }
-//                 _ => {
-//                     ()
-//                 }
-//             }
-//
-//             return quote! {
-//                 #ast
-//             }.into();
-//         }
-//         _ => panic!("`add_field` has to be used with structs "),
-//     }
-// }
